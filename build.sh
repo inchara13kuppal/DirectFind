f1d97ca4ed644eb4d73e50cc5cc3d14326f8d1cd
Inchara K Kuppal <154305916+inchara13kuppal@users.noreply.github.com>
2026-01-04 15:41:53 +0000

    Make build script executable

--- build.sh
+++ build.sh
@@ -6,3 +6,5 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+python manage.py shell -c "from recommender.views import run_manual_upload; run_manual_upload()"
